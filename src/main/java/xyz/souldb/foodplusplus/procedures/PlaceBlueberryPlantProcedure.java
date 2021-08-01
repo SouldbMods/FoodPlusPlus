04b56b653b5e49e49c3d8f94e33ebdedcd70e144
SouldbMods <teamreallyinspace@gmail.com>
2021-08-01 16:50:37 +0530

    Added sound to plant placing and fixed breaking unbreakible blocks

--- src/main/java/xyz/souldb/foodplusplus/procedures/PlaceBlueberryPlantProcedure.java
+++ src/main/java/xyz/souldb/foodplusplus/procedures/PlaceBlueberryPlantProcedure.java
@@ -3,8 +3,13 @@ package xyz.souldb.foodplusplus.procedures;
 import xyz.souldb.foodplusplus.block.BlueGrow1Block;
 import xyz.souldb.foodplusplus.FoodplusplusMod;
 
+import net.minecraftforge.registries.ForgeRegistries;
+
+import net.minecraft.world.World;
 import net.minecraft.world.IWorld;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.block.Blocks;
 
 import java.util.Map;
@@ -36,15 +41,71 @@ public class PlaceBlueberryPlantProcedure {
 		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
 		IWorld world = (IWorld) dependencies.get("world");
 		if ((!((world.getBlockState(new BlockPos((int) x, (int) (y + 1), (int) z))).getBlock() == Blocks.BEDROCK))) {
-			world.setBlockState(new BlockPos((int) x, (int) (y + 1), (int) z), BlueGrow1Block.block.getDefaultState(), 3);
-		} else if ((!((world.getBlockState(new BlockPos((int) x, (int) (y + 1), (int) z))).getBlock() == Blocks.END_PORTAL))) {
-			world.setBlockState(new BlockPos((int) x, (int) (y + 1), (int) z), BlueGrow1Block.block.getDefaultState(), 3);
-		} else if ((!((world.getBlockState(new BlockPos((int) x, (int) (y + 1), (int) z))).getBlock() == Blocks.END_PORTAL))) {
-			world.setBlockState(new BlockPos((int) x, (int) (y + 1), (int) z), BlueGrow1Block.block.getDefaultState(), 3);
-		} else if ((!((world.getBlockState(new BlockPos((int) x, (int) (y + 1), (int) z))).getBlock() == Blocks.BARRIER))) {
-			world.setBlockState(new BlockPos((int) x, (int) (y + 1), (int) z), BlueGrow1Block.block.getDefaultState(), 3);
+			world.setBlockState(new BlockPos((int) x, (int) y, (int) z), BlueGrow1Block.block.getDefaultState(), 3);
+			if (world instanceof World && !world.isRemote()) {
+				((World) world).playSound(null, new BlockPos((int) x, (int) y, (int) z),
+						(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.stem.place")),
+						SoundCategory.BLOCKS, (float) 1, (float) 1);
+			} else {
+				((World) world).playSound(x, y, z,
+						(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.stem.place")),
+						SoundCategory.BLOCKS, (float) 1, (float) 1, false);
+			}
+		} else if ((!((world.getBlockState(new BlockPos((int) x, (int) y, (int) z))).getBlock() == Blocks.END_PORTAL))) {
+			world.setBlockState(new BlockPos((int) x, (int) y, (int) z), BlueGrow1Block.block.getDefaultState(), 3);
+			if (world instanceof World && !world.isRemote()) {
+				((World) world).playSound(null, new BlockPos((int) x, (int) y, (int) z),
+						(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.stem.place")),
+						SoundCategory.BLOCKS, (float) 1, (float) 1);
+			} else {
+				((World) world).playSound(x, y, z,
+						(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.stem.place")),
+						SoundCategory.BLOCKS, (float) 1, (float) 1, false);
+			}
+		} else if ((!((world.getBlockState(new BlockPos((int) x, (int) y, (int) z))).getBlock() == Blocks.END_PORTAL))) {
+			world.setBlockState(new BlockPos((int) x, (int) y, (int) z), BlueGrow1Block.block.getDefaultState(), 3);
+			if (world instanceof World && !world.isRemote()) {
+				((World) world).playSound(null, new BlockPos((int) x, (int) y, (int) z),
+						(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("ambient.cave")),
+						SoundCategory.BLOCKS, (float) 1, (float) 1);
+			} else {
+				((World) world).playSound(x, y, z,
+						(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("ambient.cave")),
+						SoundCategory.BLOCKS, (float) 1, (float) 1, false);
+			}
+		} else if ((!((world.getBlockState(new BlockPos((int) x, (int) y, (int) z))).getBlock() == Blocks.BARRIER))) {
+			world.setBlockState(new BlockPos((int) x, (int) y, (int) z), BlueGrow1Block.block.getDefaultState(), 3);
+			if (world instanceof World && !world.isRemote()) {
+				((World) world).playSound(null, new BlockPos((int) x, (int) y, (int) z),
+						(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.stem.place")),
+						SoundCategory.BLOCKS, (float) 1, (float) 1);
+			} else {
+				((World) world).playSound(x, y, z,
+						(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.stem.place")),
+						SoundCategory.BLOCKS, (float) 1, (float) 1, false);
+			}
 		} else if ((!((world.getBlockState(new BlockPos((int) x, (int) (y + 1), (int) z))).getBlock() == Blocks.END_PORTAL_FRAME))) {
-			world.setBlockState(new BlockPos((int) x, (int) (y + 1), (int) z), BlueGrow1Block.block.getDefaultState(), 3);
+			world.setBlockState(new BlockPos((int) x, (int) y, (int) z), BlueGrow1Block.block.getDefaultState(), 3);
+			if (world instanceof World && !world.isRemote()) {
+				((World) world).playSound(null, new BlockPos((int) x, (int) y, (int) z),
+						(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.stem.place")),
+						SoundCategory.BLOCKS, (float) 1, (float) 1);
+			} else {
+				((World) world).playSound(x, y, z,
+						(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.stem.place")),
+						SoundCategory.BLOCKS, (float) 1, (float) 1, false);
+			}
+		} else if ((!((world.getBlockState(new BlockPos((int) x, (int) (y + 1), (int) z))).getBlock() == Blocks.NETHER_PORTAL))) {
+			world.setBlockState(new BlockPos((int) x, (int) y, (int) z), BlueGrow1Block.block.getDefaultState(), 3);
+			if (world instanceof World && !world.isRemote()) {
+				((World) world).playSound(null, new BlockPos((int) x, (int) y, (int) z),
+						(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.stem.place")),
+						SoundCategory.BLOCKS, (float) 1, (float) 1);
+			} else {
+				((World) world).playSound(x, y, z,
+						(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.stem.place")),
+						SoundCategory.BLOCKS, (float) 1, (float) 1, false);
+			}
 		}
 	}
 }
